a585449b6bd91041681d9f45a37078f5d1a34ef6
kallentu <kallentu@hotmail.ca>
2018-07-16 23:47:39 -0400

    Initial framework of all activities.

---
+++ app/src/main/java/com/auxil/auxil/FoodBankDonateActivity.java
@@ -0,0 +1,13 @@
+package com.auxil.auxil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoodBankDonateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_bank_donate);
+    }
+}
