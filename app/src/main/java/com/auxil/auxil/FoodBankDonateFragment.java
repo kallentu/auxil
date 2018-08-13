aa91d60d9adad4bb0694eb7b4c55cd66f1a4c7ad
kallentu <kallentu@hotmail.ca>
2018-08-13 02:53:44 -0400

    Added framework of Amazon Purchasing API.

--- app/src/main/java/com/auxil/auxil/FoodBankDonateFragment.java
+++ app/src/main/java/com/auxil/auxil/FoodBankDonateFragment.java
@@ -12,6 +12,8 @@ public class FoodBankDonateFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
         return inflater.inflate(R.layout.fragment_food_bank_donate, container);
     }
 }
