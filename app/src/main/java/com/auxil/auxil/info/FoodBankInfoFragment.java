b8527392bc1976cde10c387841dac98436574743
kallentu <kallentu@hotmail.ca>
2018-08-14 00:44:52 -0400

    Added list adapter framework for required food bank foods.

--- app/src/main/java/com/auxil/auxil/info/FoodBankInfoFragment.java
+++ app/src/main/java/com/auxil/auxil/info/FoodBankInfoFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import com.auxil.auxil.R;
 
@@ -14,6 +16,19 @@ public class FoodBankInfoFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_food_bank_info, container);
+        LinearLayout view = (LinearLayout) inflater.inflate(R.layout.fragment_food_bank_info, container);
+        setUpRequiredFoodListAdapter(view);
+        return view;
+    }
+
+    /**
+     * Sets up {@link RequiredFoodListAdapter}.
+     * Used to set up the food information that food banks want the most.
+     */
+    private void setUpRequiredFoodListAdapter(View view) {
+        ListView infoList = view.findViewById(R.id.food_bank_required_foods_list);
+        RequiredFoodListAdapter listAdapter =
+                new RequiredFoodListAdapter(view.getContext(), R.layout.adapter_info);
+        infoList.setAdapter(listAdapter);
     }
 }
