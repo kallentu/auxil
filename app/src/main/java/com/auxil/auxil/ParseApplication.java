4c4cfb68376f80a9ba07aa8e1537f456480987db
kallentu <kallentu@hotmail.ca>
2018-08-18 18:37:30 -0400

    Added ParseApplication and changed Manifest for the application name, framework for the parser.

---
+++ app/src/main/java/com/auxil/auxil/ParseApplication.java
@@ -0,0 +1,12 @@
+package com.auxil.auxil;
+
+import android.app.Application;
+
+public class ParseApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Insert Parse.initialize() here
+    }
+}
