7aa0c648e4cea208b53a08558231df3810fbd956
kallentu <kallentu@hotmail.ca>
2018-08-31 10:36:03 -0400

    Changed FAQActivity to FAQFragment, as well as fragment_faq

--- app/src/main/java/com/auxil/auxil/faq/FAQFragment.java
+++ app/src/main/java/com/auxil/auxil/faq/FAQFragment.java
@@ -1,20 +1,21 @@
-package com.auxil.auxil;
+package com.auxil.auxil.faq;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.auxil.auxil.R;
+
 /** Fragment for frequently asked questions and more explanation on Auxil. */
-public class FAQActivity extends Fragment {
+public class FAQFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_faq, container, false);
+        return inflater.inflate(R.layout.fragment_faq, container, false);
     }
 }
